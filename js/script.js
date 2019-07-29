3966052325dff463ee241d810e9a36a80165059e
HamzaJirah <hamzajirah@gmail.com>
2019-07-29 07:21:17 +0100

    selected DOM elements

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// get reference to DOM elements
+let playerScore = document.querySelector('[data-player]'),
+    computerScore = document.querySelector('[data-computer]');
+
+const choices = document.querySelectorAll('.choice');
+console.log(choices);
