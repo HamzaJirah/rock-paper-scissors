dea979ff2ec5e07da48c31ceccd86f32646da586
HamzaJirah <hamzajirah@gmail.com>
2019-07-29 14:12:04 +0100

    refactored javascript logic

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
 // get reference to DOM elements
 let playerScore = document.querySelector('[data-player]'),
     computerScore = document.querySelector('[data-computer]'),
-    player = playerChoice();
-    computer = computerChoice();
-
+    computer_choice = randomSelection();
+  
 const choices = document.querySelectorAll('.game-choice'),
       modal = document.querySelector('.modal');
 
-// Create an empty object 
-const gameChoice = {
-
-}
 // Play game 
 function playGame () {
   makeChoice();
 }
 
+// convert nodelist to array
+const gameChoice = Array.from(choices);
 // loop through the choices
-choices.forEach(choice => {
+gameChoice.forEach(choice => {
   choice.addEventListener('click', makeChoice)
 })
 
 // make choice
-function makeChoice (choose) {
-  
+function makeChoice (player_choice) {
+  if(player_choice.target.classList.contains('fas')){
+    modal.style.display = 'block';
+  } 
 }
 
+// random number
+function randomSelection(){
+  return Math.floor(Math.random() * 3);
+}
+
+
+// close modal 
+modal.addEventListener('click', e => {
+  if(e.target.className === 'modal'){
+    modal.style.display = 'none';
+  }
+})
 
