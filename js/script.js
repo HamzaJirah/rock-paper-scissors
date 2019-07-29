91ffdc384bb776a55719153b5abd9e0b7a5f7687
HamzaJirah <hamzajirah@gmail.com>
2019-07-29 20:52:27 +0100

    display message to UI

--- js/script.js
+++ js/script.js
@@ -3,58 +3,56 @@ let playerScore = document.querySelector('[data-player]'),
     computerScore = document.querySelector('[data-computer]');
   
 const choices = document.querySelectorAll('.game-choice'),
-      choices_1 = document.querySelectorAll('.choice .fas'),
       modal = document.querySelector('.modal'),
       modalContent = document.querySelector('.modal-content');
 
 // Play game 
 function playGame () {
-  makeChoice();
-  compRandomChoice();
+  // makeChoice();
+  gameRandomChoice();
 }
 
-// convert nodelist to array
-const gameChoice = Array.from(choices);
-// loop through the choices
-gameChoice.forEach(choice => {
-  choice.addEventListener('click', makeChoice)
-})
-
-// make choice
-function makeChoice (player_choice) {
-  if(player_choice.target.classList.contains('fas')){
-    modal.style.display = 'block';
-    modalContent.innerHTML = `
-      <h1>Developing game...</h1>
-      <i class="${player_choice.target.classList}"></i>
-    `;
-  }
-}
 
 // computer random choice
-choices_1.forEach(choice => {
-  choice.addEventListener('click', compRandomChoice);
+choices.forEach(choice => {
+  choice.addEventListener('click', gameRandomChoice);
 })
 
-function compRandomChoice(e){
-  let randomNum =  Number(Math.floor(Math.random() * 3));
+function gameRandomChoice(e){
+  let randomNum =  Math.floor(Math.random() * 3);
   let randomChoice = e.target;
   if(randomChoice && randomNum === 0){
+    modal.style.display = 'block';
+    modalContent.innerHTML = `
+      <h1>You lost</h1>
+      <p>Computer Chose ⬇️</p>
+      <i class="${randomChoice.classList}"></i>
+    `;
+    playerScore--;
+    computerScore++;
     console.log(`You lost ${randomNum}`)
   } else if (randomChoice && randomNum  === 1) {
-    console.log(`You win ${randomNum}`)
+      modal.style.display = 'block';
+      modalContent.innerHTML = `
+        <h1>You Won</h1>
+        <p>Computer Chose ⬇️</p>
+        <i class="${randomChoice.classList}"></i>
+      `;
+      playerScore++;
+      computerScore--;
+      console.log(`You win ${randomNum}`)
   } else if(randomChoice && randomNum === 2) {
-    console.log(`You drew ${randomNum}`)
+      modal.style.display = 'block';
+      modalContent.innerHTML = `
+        <h1>You drew</h1>
+        <p>Computer Chose ⬇️</p>
+        <i class="${randomChoice.classList}"></i>
+      `;
+      playerScore -= 2;
+      computerScore -= 2;
+      console.log(`You drew ${randomNum}`)
   } 
 }
-
-
-
-// random number
-// function randomSelection(){
-//   return Math.floor(Math.random() * 3);
-// }
-
 
 // close modal 
 modal.addEventListener('click', e => {
